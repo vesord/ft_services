6dbaf6ebc50248154b33b6bd269f759d13621746
Matrus <matrus@student.21-school.ru>
2020-09-28 14:07:25 +0300

    ftps

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 #    By: matrus <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/09/22 10:23:09 by matrus            #+#    #+#              #
-#    Updated: 2020/09/26 00:26:09 by matrus           ###   ########.fr        #
+#    Updated: 2020/09/28 13:15:27 by matrus           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,11 +17,15 @@ minikube addons enable metallb
 kubectl apply -f srcs/metallb/metallb_config.yaml
 eval $(minikube docker-env)
 
-docker build -t wordpress:matrus ./srcs/wordpress
-kubectl apply -f ./srcs/wordpress/wordpress.yaml
-docker build -t mysql:matrus ./srcs/mysql
-kubectl apply -f ./srcs/mysql/pv.yaml
-kubectl apply -f ./srcs/mysql/mysql.yaml
-docker build -t phpmyadmin:matrus ./srcs/phpma
-kubectl apply -f ./srcs/phpma/phpmyadmin.yaml
+#docker build -t wordpress:matrus ./srcs/wordpress
+#kubectl apply -f ./srcs/wordpress/wordpress.yaml
 
+#docker build -t mysql:matrus ./srcs/mysql
+#kubectl apply -f ./srcs/mysql/pv.yaml
+#kubectl apply -f ./srcs/mysql/mysql.yaml
+
+#docker build -t phpmyadmin:matrus ./srcs/phpma
+#kubectl apply -f ./srcs/phpma/phpmyadmin.yaml
+
+docker build -t ftps:matrus ./srcs/ftps
+kubectl apply -f ./srcs/ftps/ftps.yaml
